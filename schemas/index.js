c1dc02071b923eff789a0727755b5ac577df6848
ΛRSHΛQ SHΛZLY <mjmarshaq@gmail.com>
2023-01-18 18:25:52 +0530

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+export const schemaTypes = [
+    works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+]
